1869188e369b20bdb25f3f9e229ded2abe4449b4
Mauricio Diaz <mrcdz@pm.me>
2021-11-29 22:00:29 -0700

    Progress on IsPalindrome method

--- PalindromicPrime.cs
+++ PalindromicPrime.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace ActivityE5
 {
-    class PrimePalindrome
+    class PalindromicPrime
     {
         public bool IsPrime(int number)
         {
@@ -19,10 +19,21 @@ namespace ActivityE5
         }
         public bool IsPalindrome(string input)
         {
-            
+            bool isPalindrome;
             string[] arrayChars = new string[input.Length];
-            
+            string[] reverseArrayChars = new string[input.Length];
 
+            for(int i = 0; i < input.Length; i++)
+            {
+                arrayChars[i] = input.Substring(i,1);
+            }
+            int j = 0;
+            for(int i = arrayChars.Length-1; i >= 0; i--)
+            {
+                reverseArrayChars[j] = arrayChars[i];
+                j++;
+            }
+            
         }
     }
 }
